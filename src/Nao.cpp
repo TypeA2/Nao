7e94a0091aa9af07c6b564c2db9b9342f3b39633
TypeA2 <nuanlim29@gmail.com>
2020-12-20 01:56:37 +0100

    basic window

--- src/Nao.cpp
+++ src/Nao.cpp
@@ -1,8 +1,17 @@
-﻿#include "Nao.h"
+﻿#include "window.h"
 
-#include "libnao-ui.h"
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd) {
+    nao::window w;
 
-int main() {
-	std::cout << "Hello CMake." << foo() << std::endl;
-	return 0;
+    while (true) {
+        MSG msg;
+        while (PeekMessageW(&msg, nullptr, 0, 0, PM_REMOVE)) {
+            if (msg.message == WM_QUIT) {
+                return msg.wParam;
+            }
+
+            TranslateMessage(&msg);
+            DispatchMessageW(&msg);
+        }
+    }
 }
